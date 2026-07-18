fb7e602ea2c1da3c99be88462d500f88e2119a86
vitorspada <vitorspada.rs@gmail.com>
2026-07-18 18:14:48 -0300

    adicionando o decorator de tempo de execução e de tentativas

---
+++ timer_decorator.py
@@ -0,0 +1,13 @@
+from loguru import logger
+import time
+from functools import wraps
+
+def time_measure_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        logger.info(f"Função '{func.__name__}' executada em {end_time - start_time:;4f} segundos")
+        return result
+    return wrapper
